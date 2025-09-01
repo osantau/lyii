b251b15f2ac078639aeac2be36ddd4e7ae8c01ac
osantau <octavian.santau.os@gmail.com>
2025-09-01 22:56:04 +0300

    Actualziare ActiveForm. Utilizare clasa din yii\bootstrap5

--- views/driver/_form.php
+++ views/driver/_form.php
@@ -1,11 +1,11 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use yii\bootstrap5\ActiveForm;
 
 /** @var yii\web\View $this */
 /** @var app\models\Driver $model */
-/** @var yii\widgets\ActiveForm $form */
+/** @var yii\bootstrap5\ActiveForm $form */
 ?>
 
 <div class="driver-form">
--- views/user/_form.php
+++ views/user/_form.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use yii\bootstrap5\ActiveForm;
 use app\models\User;
 
 /** @var yii\web\View $this */
