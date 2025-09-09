3092de89fe64e63870c6b4fc3d9a0fdcf512becc
osantau <octavian.santau@plastor.ro>
2025-09-09 12:36:11 +0300

    Schimbare Nume aplicatie

--- config/web.php
+++ config/web.php
@@ -11,7 +11,7 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
-    'name'=>'Data Manager',
+    'name'=>'Trs Flota',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
