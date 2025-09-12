9c49dae7d29a64cc86ca1047551cb96fb5053300
osantau <octavian.santau@plastor.ro>
2025-09-12 08:54:39 +0300

    Adaugare logout button in pagina principala

--- views/site/index.php
+++ views/site/index.php
@@ -5,7 +5,7 @@
 $this->title = Yii::$app->name;
 ?>
 <div class="site-index">
-
+<?php if (Yii::$app->user->isGuest): ?>
    <div class="jumbotron text-center bg-transparent">
       <h1 class="display-4"><?= $this->title ?></h1>
       <div class="alert alert-info" role="alert">
@@ -13,4 +13,12 @@ $this->title = Yii::$app->name;
       <a href="/site/login" class="btn btn-lg btn-success">Autentificare</a>
 </div>
    </div>
+   <?php else: ?>
+       <div class="jumbotron text-center bg-transparent">
+      <h1 class="display-4"><?= $this->title ?></h1>
+      <div class="alert alert-info" role="alert">     
+      <a href="/site/logout" data-method="post" class="btn btn-lg btn-danger">Deconectare</a>
+</div>
+   </div>
+   <?php endif; ?>
 </div>
