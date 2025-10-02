fde4e736a10ca19c365fa94e534dd22703f74fb2
osantau <octavian.santau@plastor.ro>
2025-10-02 22:58:11 +0300

    Adaugare sofer la info

--- views/vehicle/_popover.php
+++ views/vehicle/_popover.php
@@ -17,7 +17,10 @@ if (isset($model->end_date)) {
             . $model->imp_adr_end . '</div>';
     } */
 }
-
+if (isset($model->driver))
+{
+    $popoverContent .= '<div><strong>Sofer:</strong> '.$model->driver.'</div>';   
+}
 $popoverContent .= '</div>';
 echo $popoverContent;
 ?>
